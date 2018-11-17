6f16b88e901fa4535ca0a8486e63ea42499f1297
Nicolas Duque <you@example.com>
2018-11-17 12:57:56 -0800

    Defino e inicializo dos arreglos de posicion y velocoidad

--- ODE.cpp
+++ ODE.cpp
@@ -22,3 +22,37 @@ float t, dt, tf;
 t = 0.0;
 tf = 5.0;
 dt = 0.0001;
+
+
+//Defino un puntero que voy a usar mas tarde como la norma de un vector con dos entradas
+
+float norma(float *v)
+{ 
+	float norma2;
+	norma2 = pow(v[0],2) + pow(v[1],2);
+	return sqrt(norma2);
+}
+
+
+int main(){
+
+	//Creo arreglos que almacenen dos velocidades y dos posiciones, como x (posicion) y v (velocidad)
+	float x[2];
+
+	float v[2];
+
+	x[0] = 0.0; x[1]=0.0;
+
+	v[0] = 300.0*cos(2*3.1416*45.0/360.0);
+
+	v[1] = 300.0*sin(2*3.1416*45.0/360.0);
+
+
+
+
+
+
+
+
+
+}
