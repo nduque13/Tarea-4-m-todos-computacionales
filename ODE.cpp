ac5bc06d96452d9e5306bcd587c745fcc0e8119e
Nicolas Duque <you@example.com>
2018-11-17 11:25:50 -0800

    Primero

---
+++ ODE.cpp
@@ -0,0 +1,24 @@
+//Comienza el primer punto de la tarea 4 de metodos computacionales. ODEs
+
+
+//Importa las librerias necesarias 
+#include "iostream"
+#include "fstream"
+#include "cmath"
+#include "cstdio"
+
+
+
+//Crea las variables que van a ser usadas a lo largo del ejercicio, como gravedad, C y demas
+
+// Gravedad, C, masa, y el tiempo con sus diferenciales.
+float g=10.0;
+
+float c=0.2;
+float m=0.2;
+
+
+float t, dt, tf;
+t = 0.0;
+tf = 5.0;
+dt = 0.0001;
