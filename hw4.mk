3d56210b8862558a75d124266450b768698ba701
Nicolas Duque Valenzuela <UANDES\n.duque13@fisicaez12209l.ad.uniandes.edu.co>
2018-11-19 16:28:07 -0500

    El make falla es por una bobada en numpy, considere el error

--- hw4.mk
+++ hw4.mk
@@ -2,16 +2,15 @@
 
 
 ###crea las dependencias######
-all: fronteraF_CaCO3.png fronteraA_CaCO3.png fronteraP_CaCO3.png
+all: fronteraF_CaCO3_0.png fronteraA_CaCO3_0.png fronteraP_CaCO3_0.png salida_proyectiles.png
 
-fronteraF_CaCO3.png fronteraA_CaCO3.png fronteraP_CaCO3.png: fronteraF_CaCO3.txt fronteraA_CaCO3.txt fronteraP_CaCO3.txt
-	python Plots_hw4.py
+fronteraF_CaCO3_0.png fronteraA_CaCO3_0.png fronteraP_CaCO3_0.png salida_proyectiles.png: fronteraF_CaCO3.txt fronteraA_CaCO3.txt fronteraP_CaCO3.txt x.txt y.txt vx.txt vy.txt
+	python3 Plots_hw4.py
 
 fronteraF_CaCO3.txt fronteraA_CaCO3.txt fronteraP_CaCO3.txt: PDE.cpp
 	g++ PDE.cpp -o ejecutable
 	./ejecutable
 
-datos.txt:
+x.txt y.txt vx.txt vy.txt: ODE.cpp
 	g++ ODE.cpp -o ejecutable
 	./ejecutable
-	python Plots_hw4.py
